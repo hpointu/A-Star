d2c23110c4262e5114df971f06495ba6ae98f281
ekryyn <tedefump@gmail.com>
2011-05-21 17:30:39 +0200

    Création du projet

---
+++ RenderArea.cpp
@@ -0,0 +1,5 @@
+#include "RenderArea.h"
+
+RenderArea::RenderArea()
+{
+}
---
+++ RenderArea.h
@@ -0,0 +1,10 @@
+#ifndef RENDERAREA_H
+#define RENDERAREA_H
+
+class RenderArea
+{
+public:
+    RenderArea();
+};
+
+#endif // RENDERAREA_H
